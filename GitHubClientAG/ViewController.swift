932a9ae2f89314e5adf058835ab7b5a01b2a2473
Alexandre Giguere <agiguere@beyondtechnologies.ca>
2019-10-24 12:21:19 -0400

    Initial Commit

---
+++ GitHubClientAG/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubClientAG
+//
+//  Created by Alexandre Giguere on 2019-10-24.
+//  Copyright © 2019 Beyond Technologies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
